9f2c06e537b81955c7b70b8e863d956eb0792b54
axmadjon <ochilovaxmadjon298@gmail.com>
2024-03-18 17:43:20 +0500

    yangi fayl qo'shildi
    pythonda numbers haqida darsklik

---
+++ numbers_lesson.py
@@ -0,0 +1,50 @@
+#sonlar(numbers)
+a=65
+b=67.5
+print(a+b)
+print(b)
+print(type(a))
+print(type(b))
+aholi_soni=36_456_345
+print("bugungi kunda O'ZEBKISTONDA ",aholi_soni,"ta odam yashab kelmoqda")
+print(f"bugungi ukunda o'zbekistonda {aholi_soni}ta odam yashab kelmoqda")
+x,z,y=56,6.7,-89
+print(y)
+print(x*z+y)
+ism=('ahmad')
+print(type(ism))
+a=5.7
+b=12
+print(a+b)
+d=100//2
+ism='Ahmad'
+yosh=36
+print("20 days are "+str(20*24*60)+" minutes")
+ism="Ahmad"
+yosh=17.6
+xabar=ism+' '+str(17.6)+' yoshda'
+print(xabar)
+name='Abbos'
+age=17
+autobiogapy=name+' '+str(age)+' yoshda'
+print(autobiogapy)
+temp=36.6
+print(temp)
+print(type(temp))
+temp=str(temp)
+print(type(temp))
+temp=float(temp)
+print(type(temp))
+a=int("20")
+b=float("10")
+c=str(37.8)
+print(type(a))
+
+
+
+
+
+
+
+
+
